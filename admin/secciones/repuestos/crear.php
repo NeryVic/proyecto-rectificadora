a17ed6474bd2a4fe9cfbc1bc8ba1d5baaf052aac
ALEJANDRO PINTOS. <148916132+NOVATCODER@users.noreply.github.com>
2024-06-06 19:35:55 -0300

    Secciones/repuestos 30 porciento

--- admin/secciones/repuestos/crear.php
+++ admin/secciones/repuestos/crear.php
@@ -1,3 +1,55 @@
-<?php include("../../templates/header.php");?> 
-crear Repuestos
+<?php 
+
+
+include("../../templates/header.php");?> 
+</br>
+<div class="card">
+    <div class="card-header">Producto/Repuesto</div>
+    <div class="card-body">
+        <form action="" enctype="multipart/form-data" method="post">
+
+            <div class="mb-3">
+                <label for="imagen" class="form-label">Imágen:</label>
+                <input
+                    type="file"
+                    class="form-control"
+                    name="imagen"
+                    id="imagen"
+                    placeholder="Imagen"
+                    aria-describedby="fileHelpId"
+                />
+            </div>
+
+            <div class="mb-3">
+                <label for="descripcion" class="form-label">Título:</label>
+                <input
+                    type="text"
+                    class="form-control"
+                    name="titulo"
+                    id="titulo"
+                    aria-describedby="helpId"
+                    placeholder="Título"
+                />
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">Descripción:</label>
+                <input
+                    type="text"
+                    class="form-control"
+                    name="descripcion"
+                    id="descripcion"
+                    aria-describedby="helpId"
+                    placeholder="Descripcion"
+                />
+                
+            </div>
+            
+
+            <button type="submit" class="btn btn-success">Agregar</button>
+            <a class="btn btn-primary" href="index.php" role="button">Cancelar</a>
+        </form>
+    </div>
+    <div class="card-footer text-muted"></div>
+</div>
+
 <?php include("../../templates/foother.php");?>
--- admin/secciones/repuestos/index.php
+++ admin/secciones/repuestos/index.php
@@ -1,3 +1,82 @@
-<?php include("../../templates/header.php");?> 
-listar Repuestos
+<?php 
+include("../../bd.php");
+
+if(isset($_GET['txtID'])){
+    //borrar dicho registro con el ID correspondiente.
+    
+    $txtID=(isset($_GET['txtID']) )?$_GET['txtID']:"";
+    $sentencia=$conexion->prepare("DELETE FROM `tabla_repuestos` WHERE id=:id ");
+
+    $sentencia->bindParam(":id",$txtID);
+
+    $sentencia->execute();
+}
+
+//seleccionar registros
+$sentencia=$conexion->prepare("SELECT * FROM `tabla_repuestos`");
+$sentencia->execute();
+$lista_repuestos = $sentencia->fetchAll(PDO::FETCH_ASSOC);
+
+
+include("../../templates/header.php");?>
+</br>
+<div class="card">
+    <div class="card-header"> <a
+        name=""
+        id=""
+        class="btn btn-primary"
+        href="crear.php"
+        role="button"
+        >Agregar registro</a
+    ></div>
+    <div class="card-body">
+        <div
+            class="table-responsive-sm"
+        >
+            <table
+                class="table"
+            >
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Imágen</th>
+                        <th scope="col">Título</th>
+                        <th scope="col">Descripción</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <tr class="">
+                    <?php foreach($lista_repuestos as $registros){;?>
+                        <td scope="row"><?php echo $registros['ID'];?></td>
+                        <td><img width="50" height="50" src="../../assets/img/Repuestos/ <?php echo $registro['imagen']; ?>" alt="Imagen del portafolio"></td>
+                        <td><?php echo $registros['titulo'];?></td>
+                        <td>
+                        <a
+                            name=""
+                            id=""
+                            class="btn btn-info"
+                            href="editar.php?txtID=<?php echo $registros['ID']; ?>"
+                            role="button"
+                            >Editar</a
+                        >
+                        <a
+                            name=""
+                            id=""
+                            class="btn btn-danger"
+                            href="index.php?txtID=<?php echo $registros['ID']; ?>"
+                            role="button"
+                            >Eliminar</a
+                        >
+                        </td>
+                    </tr>
+                    <?php };?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <div class="card-footer text-muted"></div>
+</div>
+
 <?php include("../../templates/foother.php");?>
+
